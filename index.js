3a8e5c13dfae11dde2baf49ee074f41758d29b52
aktarulrahul <contact.aktarul@gmail.com>
2021-11-07 11:46:00 +0600

    implement post and put API of register user

--- index.js
+++ index.js
@@ -31,13 +31,38 @@ async function mongodbCURD() {
     ------------------------------------- */
     const database = client.db('doctorPortal');
     const appointmentCollection = database.collection('appointments');
+    const userCollection = database.collection('users');
     /* ------------------------------------- 
     Appointments APIs
     ------------------------------------- */
+    app.get('/appointments', async (req, res) => {
+      const email = req.query.email;
+      const date = new Date(req.query.date).toLocaleDateString();
+      const query = { email: email, date: date };
+      console.log(date, email, query);
+      const cursor = appointmentCollection.find(query);
+      const appointments = await cursor.toArray();
+      res.json(appointments);
+    });
     app.post('/appointments', async (req, res) => {
       const appointment = req.body;
       const result = await appointmentCollection.insertOne(appointment);
-      console.log(result);
+      // console.log(result);
+      res.json(result);
+    });
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      // console.log(result);
+      res.json(result);
+    });
+
+    app.put('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await userCollection.updateOne(query, updateDoc, options);
       res.json(result);
     });
   } finally {
